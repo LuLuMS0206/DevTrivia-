bc39eb258899c73fc45dd874eae5846cc9dc2365
LuLuMS0206 <luciamacho00@gmail.com>
2025-01-31 12:19:37 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponentComponent } from './user-component/user-component.component';
+import { TestSelectionComponentComponent } from './test-selection-component/test-selection-component.component';
+import { TestComponentComponent } from './test-component/test-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent,
+    TestSelectionComponentComponent,
+    TestComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test-selection-component/test-selection-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-selection-component',
+  templateUrl: './test-selection-component.component.html',
+  styleUrls: ['./test-selection-component.component.css']
+})
+export class TestSelectionComponentComponent {
+
+}

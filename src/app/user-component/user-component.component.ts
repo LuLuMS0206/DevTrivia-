85d5a4334720395dbe17569d0eecdc8a07ee1f09
LuLuMS0206 <luciamacho00@gmail.com>
2025-02-06 13:00:16 +0100

    add register user

--- src/app/user-component/user-component.component.ts
+++ src/app/user-component/user-component.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-component',
   templateUrl: './user-component.component.html',
   styleUrls: ['./user-component.component.css']
 })
-export class UserComponentComponent {
+export class UserComponent {
+  username: string = '';  
+  errorMessage: string = ''; 
 
+  constructor(private router: Router) {}
+
+  validateUsername() {
+    if (this.username.trim() === '') {
+      this.errorMessage = 'Por favor, ingrese un nombre de usuario.';
+    } else if (this.username.length < 4) {
+      this.errorMessage = 'El usuario debe tener al menos 4 caracteres.';
+    } else {
+      this.errorMessage = ''; 
+    }
+  }
+
+  onSubmit() {
+    this.validateUsername(); 
+
+    if (this.errorMessage === '') {
+      localStorage.setItem('username', this.username);
+      this.router.navigate(['/test-selection']);
+    }
+  }
 }

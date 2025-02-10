93dc4b492a3981cd49c2e04b5bc59a81d9936eab
LuLuMS0206 <luciamacho00@gmail.com>
2025-02-10 11:36:51 +0100

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user-component/user-component.component';
+import { TestSelectionComponentComponent } from './test-selection-component/test-selection-component.component';  // Asegúrate de importar el componente
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
+    TestSelectionComponentComponent,  
   ],
   imports: [
     BrowserModule,

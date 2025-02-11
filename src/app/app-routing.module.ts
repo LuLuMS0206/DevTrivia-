34a70ce3a3037a6eaf282dd647248ff97cbaf47e
LuLuMS0206 <luciamacho00@gmail.com>
2025-02-11 11:55:49 +0100

    add console.log

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user-component/user-component.component';
 import { TestSelectionComponentComponent } from './test-selection-component/test-selection-component.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { ResultComponent } from './result/result.component';
 
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'tests', component: TestSelectionComponentComponent },
   { path: 'test/:testId', component: TestSelectionComponentComponent }, 
   { path: 'test/:type/start', component: TestComponentComponent },
+  { path: 'result', component: ResultComponent },
   { path: '', redirectTo: '/user', pathMatch: 'full' }
 ];
 

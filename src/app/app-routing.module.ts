ffda15313113cbf7b5b7c2cd1e3a7d89c3ad8ec0
LuLuMS0206 <luciamacho00@gmail.com>
2025-02-11 10:01:05 +0100

    add testType

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user-component/user-component.component';
 import { TestSelectionComponentComponent } from './test-selection-component/test-selection-component.component';
-// import { TestComponent } from './test-component/test-component.component';
+import { TestComponentComponent } from './test-component/test-component.component';
 
 
 const routes: Routes = [
   { path: 'user', component: UserComponent },
   { path: 'tests', component: TestSelectionComponentComponent },
   { path: 'test/:testId', component: TestSelectionComponentComponent }, 
+  { path: 'test/:type/start', component: TestComponentComponent },
   { path: '', redirectTo: '/user', pathMatch: 'full' }
 ];
 
--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TestService } from './../test.service'; 
 
 @Component({
   selector: 'app-test-component',
   templateUrl: './test-component.component.html',
   styleUrls: ['./test-component.component.css']
 })
-export class TestComponentComponent {
+export class TestComponentComponent implements OnInit {
+  testType: string = '';  
+  questions: any[] = [];  
+  currentQuestionIndex: number = 0;  
 
+  constructor(
+    private route: ActivatedRoute,
+    private testService: TestService  
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.testType = params['type'];  
+      this.loadQuestions();
+    });
+  }
+
+  loadQuestions(): void {
+    this.testService.getQuestions(this.testType).subscribe(
+      (questions) => {
+        this.questions = questions;
+      },
+      (error) => {
+        console.error('Error cargando preguntas:', error);
+      }
+    );
+  }
+
+  nextQuestion(): void {
+    if (this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex++;
+    }
+  }
+
+  previousQuestion(): void {
+    if (this.currentQuestionIndex > 0) {
+      this.currentQuestionIndex--;
+    }
+  }
 }
